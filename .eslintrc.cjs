90db75127c7ce2ec8345f4f2d6f954c40ae4fba4
Hosein Soleymani <hoseinsolleymani@gmail.com>
2023-05-26 14:48:20 +0330

    feat: added basic project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+const {init} = require("@fullstacksjs/eslint-config/init");
+
+module.exports = init({
+    modules: {
+        auto: true,
+        import: true,
+        typescript: {
+            parserProject: ["./tsconfig.json"],
+            resolverProject: ["./tsconfig.json"],
+        },
+    },
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto",
+            },
+        ],
+        "global-require": 0,
+        "@typescript-eslint/no-unsafe-enum-comparison": "off"
+    },
+});
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Appإ</div>;
+};
+
+export default App;
